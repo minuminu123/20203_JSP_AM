eb5fe6a180f2910405db32c3e57d70e2fa6f453b
minuminu123 <alsdn96321@gmail.com>
2023-04-06 15:30:11 +0900

    파라미터에 따라서 색상변경

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -1,40 +1,41 @@
 package com.KoreaIT.java.jam;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/home/main/printDan")
+@WebServlet("/printDan")
 public class HomeMainServlet extends HttpServlet {
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Hello World! sfd ").append(request.getContextPath());
 		response.setContentType("text/html; charset=UTF-8");
+
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
-		
-		if(inputedDan == null) {
+		String inputedColor = request.getParameter("color");
+
+		if (inputedDan == null) {
 			inputedDan = "1";
 		}
-		
-		if(inputedLimit == null) {
+		if (inputedLimit == null) {
 			inputedLimit = "1";
 		}
-		
-		int i = Integer.parseInt(inputedDan);
+		if (inputedColor == null) {
+			inputedColor = "black";
+		}
+
+		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
 
-		for (int j = 1; j <= limit; j++) {
-			
-			response.getWriter().append(String.format("%d * %d = %d<br>", i,j,i*j));
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+		response.getWriter().append(String.format("<div style=\"color:%s;\">==%d단==</div>", inputedColor, dan));
+
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+			response.getWriter().append(
+					String.format("<div style=\"color:%s;\">%d * %d = %d</div>", inputedColor, dan, i, dan * i));
 		}
-		
 	}
 
 }
